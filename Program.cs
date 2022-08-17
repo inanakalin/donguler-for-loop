97f8a07c4cbcd6dce2f43c40dc6909c6fc6673ae
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-08-18 01:18:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+{{    private static void Main(string[] args)
+    {
+        // ekrandan girilen sayıya kadar olan tek sayıları yazdır
+        Console.WriteLine("lütfen bir sayı giriniz");
+
+        int sayac = int.Parse(Console.ReadLine());
+        for (int i  = 1; i <= sayac; i++)
+        {
+            //komutlar
+            if ( i % 2 ==1)
+            Console.WriteLine(i);
+        }
+        // 1 ile 1000 arasındaki tek ve çift sayıların kendi içindeki toplamını ekrana yazdır
+            int tektoplam = 0;
+            int cifttoplam = 0;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i%2 == 1)
+            tektoplam += i;
+            else 
+            cifttoplam += i;
+        }
+        Console.WriteLine("tek Toplam: " + tektoplam);
+        Console.WriteLine("Çift Toplam: " + cifttoplam);}}
+
+        
+       
+ 
+
+
+
+
+
+
+
+
+    }
+}
